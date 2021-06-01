56f4ebab5dc4e34c08ff0b9bff852e786b4b4e4e
Jorge Hurtado <jjhurtado796@gmail.com>
2021-06-01 13:43:32 -0400

    Repos Creados

---
+++ src/main/java/org/jobits/pos/client/tennant/core/repo/BaseDatosRepo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jobits.pos.client.tennant.core.repo;
+
+import org.jobits.pos.client.tennant.core.domain.BaseDatos;
+
+/**
+ *
+ * JoBits
+ *
+ * @author Jorge
+ *
+ */
+public interface BaseDatosRepo extends com.root101.clean.core.app.repo.CRUDRepository<BaseDatos> {
+
+}
