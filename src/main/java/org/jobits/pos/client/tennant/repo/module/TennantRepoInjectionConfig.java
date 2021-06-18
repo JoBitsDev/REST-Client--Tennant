8f3e76f41812156d1a7f66c776d895630f86ea8f
Jorge Hurtado <jjhurtado796@gmail.com>
2021-06-17 20:16:27 -0400

    injection repo

--- src/main/java/org/jobits/pos/client/tennant/repo/module/TennantRepoInjectionConfig.java
+++ src/main/java/org/jobits/pos/client/tennant/repo/module/TennantRepoInjectionConfig.java
@@ -7,8 +7,12 @@ package org.jobits.pos.client.tennant.repo.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import org.jobits.pos.client.tennant.core.repo.BaseDatosRepo;
 import org.jobits.pos.client.tennant.core.repo.CuentaRepo;
+import org.jobits.pos.client.tennant.core.repo.TokenRepo;
+import org.jobits.pos.client.tennant.repo.impl.BaseDatosRepoImpl;
 import org.jobits.pos.client.tennant.repo.impl.CuentaRepoImpl;
+import org.jobits.pos.client.tennant.repo.impl.TokenRepoImpl;
 
 /**
  *
@@ -22,6 +26,8 @@ class TennantRepoInjectionConfig extends AbstractModule {
     @Override
     protected void configure() {
         bind(CuentaRepo.class).to(CuentaRepoImpl.class).in(Singleton.class);
+        bind(BaseDatosRepo.class).to(BaseDatosRepoImpl.class).in(Singleton.class);
+        bind(TokenRepo.class).to(TokenRepoImpl.class).in(Singleton.class);
     }
 
 }
